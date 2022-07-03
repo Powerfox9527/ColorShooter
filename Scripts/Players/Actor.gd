a14307b6f1f24ae3a69d0b1f4e40e0fb69224a60
Powerfox9527 <2504488720@qq.com>
2022-07-03 08:46:39 +0800

    fall

--- Scripts/Players/Actor.gd
+++ Scripts/Players/Actor.gd
@@ -19,6 +19,7 @@ export (float) var attack = 10
 export (float) var defense = 20
 export var color = Color(0, 0, 0, 1)
 var state = ""
+var is_falling = false
 
 func _ready():
 	Util.set_color(sprite.material, color)
@@ -31,17 +32,24 @@ func _ready():
 func _process(delta):
 	var cell = navigation.get_cell_by_pos(get_global_position())
 	var cells = generator.island_cells[0]
-	if not cells.has(cell):
-		get_hurt(Color(0, 0, 0, 0.1))
+	if not cells.has(cell) and not is_falling:
+		is_falling = true
 		var distance = INF
 		var closest_pos = Vector2.ZERO
+		yield(get_tree().create_timer(0.2), "timeout")
+		set_process(false)
+		set_physics_process(false)
 		for ok_cell in cells:
 			var ok_pos = navigation.get_pos_by_cell(ok_cell)
 			if navigation.get_cell_value(ok_cell.x, ok_cell.y) == 1 and (ok_pos - get_global_position()).length() < distance:
 				distance = (ok_pos - get_global_position()).length()
 				closest_pos = ok_pos
-		# yield(get_tree().create_timer(0.3), "timeout")
-		# set_global_position(closest_pos)
+		yield(get_tree().create_timer(0.5), "timeout")
+		set_global_position(closest_pos)
+		is_falling = false
+		get_hurt(Color(1, 1, 1, 1))
+		set_process(true)
+		set_physics_process(true)
 
 func set_color(new_color):
 	color.r = min(max(0, new_color.r), 1)
